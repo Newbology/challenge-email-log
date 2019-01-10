dc102b36630a86eaa23d9b8b78f8b830003938d1
kevin tauzer <kevint@kevins-MBP.home>
2019-01-10 10:59:48 -1000

    return list of emails sent

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-"use strict"
+'use strict';
 let dataset = require('./dataset.json');
 
-let emails = dataset.emails
+let emails = dataset.emails;
+
+let sortEmails = emails.reduce((accum, current) => {
+  if(accum.hasOwnProperty(current.email)){
+    accum[current.email]++;
+  }else{
+    accum[current.email] = 1;
+  }
+  return accum
+}, {});
+console.log(sortEmails);
+console.log(emails.length)
